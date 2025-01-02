126c9e876b10257cd904654bff88080c2f58e931
jananiiramesh <reachjanu2004@gmail.com>
2025-01-02 16:45:50 +0530

    added removeDuplicatesSortedArr.c

---
+++ removeDuplicatesSortedArr.c
@@ -0,0 +1,23 @@
+int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize == 0) return 0;  
+
+    int k = 1;  
+    int new = 1;  
+    int iterate = 1;  
+    int val = nums[0];  
+
+    while (iterate < numsSize) {
+        while (iterate < numsSize && nums[iterate] == val) {
+            iterate++;
+        }
+
+        if (iterate < numsSize) {
+            nums[new] = nums[iterate];
+            new++;
+            k++;
+            val = nums[iterate]; 
+        }
+    }
+    
+    return k;
+}
