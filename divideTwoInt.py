40d3ff4fa2db829d4c393825d14e014dae1c9e39
jananiiramesh <reachjanu2004@gmail.com>
2025-01-02 16:42:31 +0530

    added divideTwoInt.py

---
+++ divideTwoInt.py
@@ -0,0 +1,28 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        sign = -1 if (dividend<0) ^ (divisor<0) else 1
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        quotient = 0
+
+        while dividend >= divisor:
+            temp_divisor = divisor
+            multiple = 1
+            while dividend >= (temp_divisor << 1):
+                temp_divisor = temp_divisor << 1
+                multiple <<= 1
+            dividend -= temp_divisor
+            quotient += multiple
+        
+        quotient *= sign
+
+        MAX = 2**31-1
+        MIN = -2**31
+
+        if quotient>MAX:
+            return MAX
+        if quotient<MIN:
+            return MIN
+        return quotient
