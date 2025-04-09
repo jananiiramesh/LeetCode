7db168cb15932e89203c381b7cbda8c3f43e2bc2
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-04-09 21:59:11 +0530

    Easy

---
+++ sqrt(x).py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0 or x == 1:
+            return x
+        low = 0
+        high = x
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return high
