fcc4a7e9870893c07c38ee6cd4a8961cadebf6d1
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-04-11 18:35:04 +0530

    Medium

--- reverseInteger.py
+++ reverseInteger.py
@@ -4,21 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        length = 0
         negative = False
         if x<0:
             negative = True
-            length = len(str(x)) - 1
             x = abs(x)
-        else:
-            length = len(str(x))
         num = 0
-        i = length - 1
-        while x//10 > 0:
-            num = num + (x%10)*10**i
+        while x > 0:
+            add = x % 10
+            num = num * 10 + add
             x = x//10
-            i -= 1
-        num = num + (x%10)*10**i
         if negative:
             num = -(num)
         if num < -2**31 or num > 2**31 - 1:
