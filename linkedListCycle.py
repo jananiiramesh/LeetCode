43cb36b24c9bb8f1eeadf0e4cd10ae62b95e0b91
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-05-27 10:49:02 +0530

    Easy

---
+++ linkedListCycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        seen = set()
+        current = head
+        while current:
+            if current in seen:
+                return True
+            seen.add(current)
+            current = current.next
+        return False
+        
