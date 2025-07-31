8f3479a4c7b88243e62869301cf742ea6e91d289
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-07-31 13:37:55 +0530

    Medium

--- uniquePaths.py
+++ uniquePaths.py
@@ -5,16 +5,54 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(n): 
-            dp[m-1][i] = 1
-        for j in range(m):
-            dp[j][n-1] = 1
-        for i in range(m-2,-1,-1):
-            for j in range(n-2,-1,-1):
-                dp[i][j] = dp[i+1][j] + dp[i][j+1]
-        return dp[0][0]
-        
+        # ##exploring recursive solution first
 
-        
-        
+        # def recursive(x, y):
+
+        #     if x == m - 1 and y == n - 1:
+        #         return 1
+
+        #     if x >= m or y >= n:
+        #         return 0
+
+        #     right = recursive(x+1,y)
+        #     down = recursive(x,y+1)
+
+        #     return right + down
+
+        # return recursive(0,0)
+
+        ##now explore memoization
+        # dp = [[-1 for _ in range(n)] for _ in range(m)]
+
+        # def recursive(x, y):
+        #     if x == m - 1 and y == n - 1:
+        #         return 1
+
+        #     if x >= m or y >= n:
+        #         return 0
+
+        #     if dp[x][y] != -1:
+        #         return dp[x][y]
+
+        #     right_paths = recursive(x+1,y)
+        #     down_paths = recursive(x,y+1)
+
+        #     dp[x][y] = right_paths + down_paths
+        #     return dp[x][y]
+
+        # return recursive(0,0)
+
+        #now tabulation
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        dp[m-1][n-1] = 1 #base case in recursion converted here
+
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if i+1 < m:
+                    dp[i][j] += dp[i+1][j]
+                if j+1 < n:
+                    dp[i][j] += dp[i][j+1]
+
+        return dp[0][0]
