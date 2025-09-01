126ac0b9aef2ce4da35384ceb1864f8b1e613c33
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-09-01 11:08:00 +0530

    Hard

---
+++ minInsertionsToMakePalindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        s1 = s
+        s2 = s[::-1]
+
+        dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+        for ind1 in range(1, len(s1) + 1):
+            for ind2 in range(1, len(s2) + 1):
+                if s1[ind1 - 1] == s2[ind2 - 1]:
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]
+                else:
+                    dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1])
+        longest_pal = dp[len(s1)][len(s2)]
+
+        return len(s) - longest_pal
+        
