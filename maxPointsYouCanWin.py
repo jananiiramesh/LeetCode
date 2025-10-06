ec44eb275f4f0d44e022f284b3d3306a5e9c618f
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-10-06 14:29:34 +0530

    Medium

---
+++ maxPointsYouCanWin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        total = sum(cardPoints)
+        n = len(cardPoints)
+        if k == n:
+            return total
+
+        window = n - k
+        curr = sum(cardPoints[:window])
+        min_sum = curr
+
+        for i in range(window, n):
+            curr += cardPoints[i] - cardPoints[i - window]
+            min_sum = min(min_sum, curr)
+
+        return total - min_sum
