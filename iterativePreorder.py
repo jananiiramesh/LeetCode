422650073c2d6a9581c5f166d008f1eb94c91ffd
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-11-16 12:20:58 +0530

    Easy

---
+++ iterativePreorder.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# using stack
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        res = []
+        node = root
+        while node or stack:
+            while node:
+                res.append(node.val)
+                stack.append(node)
+                node = node.left
+            if stack:
+                node = stack.pop()
+                node = node.right
+
+        return res
+        
